401311745804cc129fe14d05b9ba3e689161269a
OleWagenknecht <olewagenknecht9064@gmail.com>
2023-06-25 22:53:04 +0200

    pm

--- src/bib/main/domain/KundenVerwaltung.java
+++ src/bib/main/domain/KundenVerwaltung.java
@@ -2,10 +2,27 @@ package bib.main.domain;
 
 import bib.main.entities.Kunde;
 
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class KundenVerwaltung extends BenutzerVerwaltung {
     public void registrieren(Kunde einKunde){
         if (!Kundenbestand.contains(einKunde)) {
             Kundenbestand.add(einKunde);
+            try{
+                List<String> Stammdaten = Files.readAllLines(Paths.get(String.format("%s\\Kunde", System.getProperty("user.dir"))));
+                Stammdaten.add(String.format("%s,%s,%s,%s,%s,%s", einKunde.getVorname(), einKunde.getNachname(), einKunde.getId(), einKunde.getPasswort(), einKunde.getStrasse(), einKunde.getHausnr(), einKunde.getOrt(), einKunde.getPlz(), einKunde.getLand()));
+
+                FileWriter fileWriter = new FileWriter(String.format("%s\\Kunde", System.getProperty("user.dir")));
+                for(String aktuelleZeile : Stammdaten){
+                    fileWriter.write(String.format("%s\n", aktuelleZeile));
+                }
+                fileWriter.close();
+            }
+            catch(Exception Ausnahme){
+            }
         }
     }
 
--- src/bib/main/domain/MitarbeiterVerwaltung.java
+++ src/bib/main/domain/MitarbeiterVerwaltung.java
@@ -4,7 +4,11 @@ import bib.main.entities.Mitarbeiter;
 import bib.main.pm.FilePersistenceManager;
 import bib.main.pm.PersistenceManager;
 
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MitarbeiterVerwaltung extends BenutzerVerwaltung {
 
@@ -14,6 +18,19 @@ public class MitarbeiterVerwaltung extends BenutzerVerwaltung {
         try{
             if (!mitarbeiterListe.contains(einMitarbeiter)) {
                 mitarbeiterListe.add(einMitarbeiter);
+                try{
+                    List<String> Stammdaten = Files.readAllLines(Paths.get(String.format("%s\\Mitarbeiter", System.getProperty("user.dir"))));
+                    Stammdaten.add(String.format("%s,%s,%s,%s,%s,%s", einMitarbeiter.getVorname(), einMitarbeiter.getNachname(), einMitarbeiter.getId(), einMitarbeiter.getPasswort(), einMitarbeiter.getStrasse(), einMitarbeiter.getHausnr(), einMitarbeiter.getOrt(), einMitarbeiter.getPlz(), einMitarbeiter.getLand()));
+
+                    FileWriter fileWriter = new FileWriter(String.format("%s\\Mitarbeiter", System.getProperty("user.dir")));
+                    for(String aktuelleZeile : Stammdaten){
+                        fileWriter.write(String.format("%s\n", aktuelleZeile));
+                    }
+                    fileWriter.close();
+                }
+                catch (Exception Ausnahme){
+
+                }
             }
         }
         catch(Exception e){ //Exception Registrierung nicht möglich
